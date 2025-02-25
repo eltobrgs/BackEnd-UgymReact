f3e66bfaa6e813039cf649b8cfcf067954ecbcc2
elto borges <eltobrgs@MacBook-Air-de-elto.local>
2025-02-25 15:06:08 -0300

    retirando o /api e adicionando o cors pra poder fazer a requisiçao de todo canto

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import publicRoutes from './routes/public.js';
 import privateRoutes from './routes/private.js';
-
+import cors from 'cors';
 const app = express();
 
 // Middleware para JSON
 app.use(express.json());
+app.use(cors({ origin: '*' })); // Permite requisições de qualquer origem
 
 // Rotas públicas
-app.use('/api', publicRoutes);
+app.use('/', publicRoutes);
 
 // Rotas privadas
-app.use('/api', privateRoutes);
+app.use('/', privateRoutes);
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+
 });
